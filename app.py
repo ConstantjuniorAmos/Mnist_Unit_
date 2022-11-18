b03bcce68ef4af3d5703afa7e855fe4e43a2e6b7
ConstantjuniorAmos <113611279+ConstantjuniorAmos@users.noreply.github.com>
2022-11-18 08:49:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ COUNT = 0
 app = Flask(__name__)
 
 app.config["SEND_FILE_MAX_AGE_DEFAULT"]=10
-
+# La fonction index sert de porte d'entrée sur l'application
 @app.route('/')
 def index():
     return render_template('index.html')
-
+# La fonction detect est appelé lors que l'utilisateur utilise le canva pour dessiner les chiffre
 @app.route('/detect',methods=['GET','POST'])
 def detect():
     global COUNT
@@ -26,24 +26,17 @@ def detect():
    
     base64_decoded = io.BytesIO(base64.b64decode(request.form['canvasimg'].split("base64",1)[1]))
     image_reshaped = prediction(base64_decoded)
-    
-    print(image_reshaped)
-
-    # image = np.asarray(bytearray(base64_decoded), dtype="uint8")
-    # image = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    # img.save('static/uploads/imgae_{}.jpg'.format(COUNT+1))
     state= 1
     
-    return render_template('index.html', imgs =request.form['canvasimg'], state = state)
+    return render_template('index.html', imgs =request.form['canvasimg'], state = state, predict = image_reshaped)
+# cette fonction est utilisée lors que l'utilisateur upload une image
 @app.route('/upload',methods=['GET','POST'])
 def upload():
     img = request.files['imageTest']
-    # print(img['FileStorage:'])
     image_reshaped = prediction(img)
-    # imgs = img.resize((500, 500))
-    # print(image_reshaped)
+   
     state= 1
-    return render_template('index.html',imgs =img, state = state)
+    return render_template('index.html',imgs =img, state = state,predict = image_reshaped)
 
 if __name__ == '__main__':
     app.run()
